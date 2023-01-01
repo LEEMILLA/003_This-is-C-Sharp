60a7da8672d53dd8f740a8ee3432e514575f9bb3
LEEMILLA <leemilla605@gmail.com>
2023-01-01 22:42:59 +0900

    이것이 C#이다 3일차

---
+++ 03/Overflow/Overflow/MainApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue;
+
+            Console.WriteLine(a);
+
+            a = a + 1;
+
+            Console.WriteLine(a);
+
+            int b = int.MaxValue;
+
+            Console.WriteLine(b);
+        }
+    }
+}
