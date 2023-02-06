8b8f9fd70f14be76fe60a473b5ffd9a9fd5ce24c
LEEMILLA <leemilla605@gmail.com>
2023-02-06 21:54:02 +0900

    이것이 C#이다 23일차

---
+++ 03/Test1/Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test1
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            Console.WriteLine("사각형의 넓이는:");
+            int area = int.Parse(width) * int.Parse(height);
+            Console.WriteLine(area);
+
+
+        
+        
+
+        }
+
+    }
+}
