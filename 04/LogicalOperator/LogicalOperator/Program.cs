a9b664a305f4472ca7e014987a6fecc382fbcda5
LEEMILLA <leemilla605@gmail.com>
2023-02-14 22:22:19 +0900

    이것이 C#이다 27일차

--- 04/LogicalOperator/LogicalOperator/Program.cs
+++ 04/LogicalOperator/LogicalOperator/Program.cs
@@ -18,7 +18,7 @@ namespace LogicalOperator
             Console.WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");
             Console.WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");
 
-            Console.WriteLine("\nTesting ! ... ");
+            Console.WriteLine("\nTesting !! ... ");
             Console.WriteLine($"!True : {!true}");
             Console.WriteLine($"!False : {!false}");
 
