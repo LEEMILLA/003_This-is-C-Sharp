b15dc85253dd4469c50935f8a44cee3f92c30d1d
LEEMILLA <leemilla605@gmail.com>
2023-02-24 23:20:35 +0900

    이것이 C#이다 34일차

---
+++ 04/IfIf/IfIf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ifIf
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number > 0)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("0보다 큰 짝수");
+                else
+                    Console.WriteLine("0보다 큰 홀수.");
+            }
+            else
+            {
+                Console.WriteLine("0보다 같거나 작은 수.");
+            }
+          
+        }
+    }
+}
