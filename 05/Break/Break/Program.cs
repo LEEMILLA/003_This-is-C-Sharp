2c98a42d900cb3d4d142f562008fce3f3ed07a08
LEEMILLA <leemilla605@gmail.com>
2023-03-07 23:49:12 +0900

    이것이 C#이다 42일차

---
+++ 05/Break/Break/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Break
+{
+    class MainApp
+    {
+        static void Main(String[] args)
+        {
+            
+            while(true)
+            {
+                Console.Write("계속할까요? (예/아니요)");
+                string answer = Console.ReadLine();
+
+                if (answer == "아니dy")
+                    break;
+
+            }
+          
+        }
+    }
+}
