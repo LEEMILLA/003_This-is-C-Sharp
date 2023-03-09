c59460916977d03ce4f81ce969b22e846892fa49
LEEMILLA <leemilla605@gmail.com>
2023-03-10 08:12:28 +0900

    이것이 C#이다 45일차

---
+++ 05/Test01/Test01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string star = "*";
+            
+
+            Console.WriteLine("*을 몇 번 돌리겠습니까?");
+            string input = Console.ReadLine();
+            int input_number = Convert.ToInt32(input);
+
+            for (int i = 1; i < input_number; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(star);
+
+                }
+                    Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ 05/Test02/Test02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test02
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string star = "*";
+            string input = Console.ReadLine();
+
+            int input_number = Convert.ToInt32(input);
+
+            for (int i = 1; i < input_number; i--)
+            {
+                for (int j = 0; i < j; j--)
+                {
+                    Console.Write(star);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
