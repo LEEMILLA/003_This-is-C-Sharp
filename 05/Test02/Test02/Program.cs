f23595bbc2369e209811c8ec1e10f2d8a6b96c20
LEEMILLA <leemilla605@gmail.com>
2023-03-18 17:16:26 +0900

    이것이 C#이다 40일차

--- 05/Test02/Test02/Program.cs
+++ 05/Test02/Test02/Program.cs
@@ -24,7 +24,7 @@ namespace Test02
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine();
-
+                Console.WriteLine();
 
             }
         }
