029e275cccf246dafb4e5f86ace9aa10c121500e
LEEMILLA <leemilla605@gmail.com>
2023-03-21 23:02:29 +0900

    이것이 C#이다 42일차

---
+++ 05/Test03/Test03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test03
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            /* 
+             * =====================================
+             * 주석: FOR 문으로 *을 찍어본다
+             * =====================================
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            int i = 1;
+            int j = 1;
+
+            while (i < 6)
+            {
+                while (j < i)
+                {
+                    Console.Write("*");
+                    j++;
+                }
+                i++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
