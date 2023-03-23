e8ed16bcf00a5ddc29f262ae9fa6aa835aa4d87d
LEEMILLA <leemilla605@gmail.com>
2023-03-23 23:54:44 +0900

    이것이 C#이다 44일차

---
+++ 05/Test03do/Test03do/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test03do
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            int j = 0;
+
+            do
+            {
+                Console.Write("*");
+                Console.WriteLine();
+                i++;
+            }
+            while (i < 6) ;
+
+        }
+    }
+}
