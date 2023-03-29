b4ac84da2605267ccd5633104f773ef910846b1a
LEEMILLA <leemilla605@gmail.com>
2023-03-29 21:37:42 +0900

    이것이 C#이다 48일차

---
+++ 05/Test2_for/Test2_for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test2_for
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int j = 0;
+
+
+            for (i = 5; i > 0; i--)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+                
+            }
+            
+        }
+    }
+}
