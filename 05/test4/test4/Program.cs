66fbb6a26e62d20e80533db0584eae5bb0f250af
LEEMILLA <leemilla605@gmail.com>
2023-03-30 23:49:32 +0900

    이것이 C#이다 49일차

---
+++ 05/test4/test4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace test4
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            
+            int num;
+
+            Console.Write("반복 횟수를 입력하세요 :");
+            int readLine = Convert.ToInt16(Console.ReadLine());
+                
+            
+
+            if (readLine <= 0)
+            {
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+            else
+            {
+                for (int i = 1; i < readLine + 1; i++) 
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+
+                        Console.Write("*");
+                        
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
